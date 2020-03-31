a46d490fd56618a49b0d6b74915498741d9ce1e9
xhsiung <xhsiung@gmail.com>
2020-03-31 17:07:04 +0800

    fix main

--- xcargo/wasm-pack/tpl/crate/src/lib.rs
+++ xcargo/wasm-pack/tpl/crate/src/lib.rs
@@ -1,71 +1,60 @@
-#[macro_use]
-extern crate serde_derive;
+#[macro_use] extern crate serde_derive;
+#[macro_use] extern crate serde_json;
 
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use std::collections::HashMap;
+use serde_json::Value;
 
 #[wasm_bindgen(module = "/js/foo.js")]
 extern "C" {
     fn add(a: u32, b: u32) -> u32;
 }
 
-//window.addEventListener("wasmEvent",{})
-pub fn event_listener(){
-    let window = web_sys::window().unwrap();
-    let cb = Closure::wrap(Box::new(move |jobj:web_sys::CustomEvent| {
-        #[derive(Serialize,Deserialize)]
-        pub struct mData( HashMap<String,String> );
-
-        let detail = jobj.detail();
-        let obj:mData = detail.into_serde().unwrap();
-        //get_val 
-        let name = obj.0.get("name").into();
-        web_sys::console::log_1(&name);
-    }) as Box<dyn FnMut(web_sys::CustomEvent)>);
-
-    let mut event_listener = web_sys::EventListener::new();
-
-    //convert js::Function()
-    event_listener.handle_event( &cb.as_ref().unchecked_ref() );
-    window.add_event_listener_with_event_listener( "wasmEvent" , &event_listener);
-    cb.forget();
-}
-
-//window.dispatchEvent("evnet",customEvent);
 #[wasm_bindgen]
-pub fn send_json() {
-    #[derive(Serialize,Deserialize,Debug)]
-    pub struct Data( HashMap<String,String> );
-
+pub fn myjson() {
+    let data = json!({
+        "name": "alex",
+        "mobile": "0938",
+        "data": [
+                {"name":"alex"},
+                {"name":"alex2"},
+        ]
+    });
+    let jobj  = JsValue::from_serde( &data).unwrap() ;
     let window = web_sys::window().unwrap();
+
     //let event = web_sys::CustomEvent
     let mut customeventinit = web_sys::CustomEventInit::new();
-    let mut jobj = HashMap::new();
-    jobj.insert("name".to_string(),"alex".to_string());
-    jobj.insert("mobile".to_string(),"0938".to_string());
-    let obj = JsValue::from_serde( &Data( jobj ) ).unwrap() ;
-    customeventinit.detail(&obj);
+    customeventinit.detail(&jobj);
 
     let customevent = web_sys::CustomEvent::new_with_event_init_dict("event", &customeventinit);
     window.dispatch_event(&customevent.unwrap()).unwrap();
 }
 
 
-#[wasm_bindgen(start)]
-pub fn run() {
-    send_json();
-    event_listener();
-    web_sys::console::log_2(&"add:".into(),&add(1,2).into());
+pub fn mylistener(){
+    let window = web_sys::window().unwrap();
+
+    let cb = Closure::wrap(Box::new(move |jobj:web_sys::CustomEvent| {
+        let detail = jobj.detail();
+        let v:Value = detail.into_serde().unwrap();
+        //get_val
+        let name = v["name"].as_str().unwrap();
+        web_sys::console::log_1(&JsValue::from_str( name ));
+
+    }) as Box<dyn FnMut(web_sys::CustomEvent)>);
+    let mut event_listener = web_sys::EventListener::new();
+    //convert js::Function()
+    event_listener.handle_event( &cb.as_ref().unchecked_ref() );
+    window.add_event_listener_with_event_listener( "wasmEvent" , &event_listener);
+    cb.forget();
 }
 
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_myadd() {
-        //assert_eq!( myadd(1,2), 1);
-    }
+#[wasm_bindgen(start)]
+pub fn run() {
+    myjson();
+    mylistener();
+    //web_sys::console::log_2(&"add:".into(),&add(1,2).into());
 }
